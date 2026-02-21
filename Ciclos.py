5b0ec7af728b35309728ca84c77a0587f83d912b
peddiazd <pedro.diazd@campusucc.edu.co>
2026-02-21 16:56:32 +0000

    CICLOS

---
+++ Ciclos.py
@@ -0,0 +1,21 @@
+def factWhile(nu)-> int:
+    if nu == 0 or nu == 1:
+        return 1
+    while nu > 1 :
+        return nu * factWhile(nu-1)
+print(factWhile(5))
+
+
+#fact for 
+def factFor(nu2)-> int:
+    acum =1
+    for i in range(2,nu2+1):
+        acum*=i
+    return acum
+print(factFor(5))
+
+
+
+
+
+
